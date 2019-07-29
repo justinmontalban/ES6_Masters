3b70f94c25e3397f90cac72ce6d2dc11e63cf2c0
justinmontalban <justin_montalban@yahoo.com>
2019-07-30 02:07:16 +0800

    Create Exercise2.js

---
+++ Exercise2/Exercise2.js
@@ -0,0 +1,71 @@
+const contestant = [
+	{ 
+		Name: 'Foo',
+		Score: 9
+
+	},
+	{ 
+		Name: 'Bar',
+		Score: 7
+
+	},
+	{ 
+		Name: 'Baz',
+		Score: 6
+		
+	},
+	{ 
+		Name: 'B3az',
+		Score: 8
+		
+	},
+	{ 
+		Name: 'Bauz',
+		Score: 8
+		
+	},
+	{ 
+		Name: 'saz',
+		Score: 8
+		
+	}
+]
+const ranking = ['(1st)','(2nd)','(3rd)'];
+const noOfWinners = 3;
+const scoreOverRate = 10;
+
+let arrageFromHighestScore = contestant => {
+	
+	return contestant.sort(function(a, b){return b.Score-a.Score});
+}
+
+
+
+let getWinners = (contestant,noOfWinners) => {
+
+const winners = arrageFromHighestScore(contestant);
+let counterRanking = 0;
+for(score =scoreOverRate; score >= 0; score--)
+{
+
+		let winnersTied = winners.filter((item) => (item.Score === score));
+   
+    
+      if(winnersTied.length !== 0){
+             	const {Name, Score} = winnersTied[0];
+        if(winnersTied.length > 1)
+       {
+
+       console.log(`${ranking[counterRanking++]} ${winnersTied.length} scored ${Score} out of ${scoreOverRate}`);
+       }
+    	else {
+       console.log(`${ranking[counterRanking++]} ${Name} scored ${Score} out of ${scoreOverRate}`);
+       
+       }
+       }
+     if(counterRanking >= 3)
+     break;
+}
+};
+
+getWinners(contestant,noOfWinners);
