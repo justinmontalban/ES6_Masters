36f5f3b536771697565167c76516bd934dfd3954
justinmontalban <justin_montalban@yahoo.com>
2019-08-03 02:39:01 +0800

    Update Exercise5.js

--- Exercise5/Exercise5.js
+++ Exercise5/Exercise5.js
@@ -1,1 +1,19 @@
+class Pokemon {
+
+   constructor({id, name, type}) {
+        this.id = id;
+        this.name = name;
+        this.type = type;
+     }
+     
+    Attack(attackname) {
+        return `${this.name} used ${attackname} and its super effective!"`;
+    }
+    
+}
+
+
+const pokemon = new Pokemon({id: 1, name: 'Bulbasaur', type: 'Grass'});
+
+console.log(pokemon.Attack('punch'));
 
